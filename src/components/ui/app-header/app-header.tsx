eedd526f20f6006a9be70c21377f7e1ec1a8b447
Daniil Sivachenko <daniil.s.2003@gmail.com>
2024-12-21 17:18:28 +0300

    review 2 fix

--- src/components/ui/app-header/app-header.tsx
+++ src/components/ui/app-header/app-header.tsx
@@ -13,30 +13,34 @@ export const AppHeaderUI: FC<TAppHeaderUIProps> = ({ userName }) => (
   <header className={styles.header}>
     <nav className={`${styles.menu} p-4`}>
       <div className={styles.menu_part_left}>
-        <>
-          <NavLink
-            to='/'
-            className={({ isActive }) =>
-              `${styles.link} ${isActive ? styles.link_active : ''}`
-            }
-          >
-            <BurgerIcon type={'primary'} />
-            <p className='text text_type_main-default ml-2 mr-10'>
-              Конструктор
-            </p>
-          </NavLink>
-        </>
-        <>
-          <NavLink
-            to='/feed'
-            className={({ isActive }) =>
-              `${styles.link} ${isActive ? styles.link_active : ''}`
-            }
-          >
-            <ListIcon type={'primary'} />
-            <p className='text text_type_main-default ml-2'>Лента заказов</p>
-          </NavLink>
-        </>
+        <NavLink
+          to='/'
+          className={({ isActive }) =>
+            `${styles.link} ${isActive ? styles.link_active : ''}`
+          }
+        >
+          {({ isActive }) => (
+            <>
+              <BurgerIcon type={isActive ? 'primary' : 'secondary'} />
+              <p className='text text_type_main-default ml-2 mr-10'>
+                Конструктор
+              </p>
+            </>
+          )}
+        </NavLink>
+        <NavLink
+          to='/feed'
+          className={({ isActive }) =>
+            `${styles.link} ${isActive ? styles.link_active : ''}`
+          }
+        >
+          {({ isActive }) => (
+            <>
+              <ListIcon type={isActive ? 'primary' : 'secondary'} />
+              <p className='text text_type_main-default ml-2'>Лента заказов</p>
+            </>
+          )}
+        </NavLink>
       </div>
       <div className={styles.logo}>
         <Logo className='' />
@@ -48,10 +52,14 @@ export const AppHeaderUI: FC<TAppHeaderUIProps> = ({ userName }) => (
             `${styles.link} ${isActive ? styles.link_active : ''}`
           }
         >
-          <ProfileIcon type={'primary'} />
-          <p className='text text_type_main-default ml-2'>
-            {userName || 'Личный кабинет'}
-          </p>
+          {({ isActive }) => (
+            <>
+              <ProfileIcon type={isActive ? 'primary' : 'secondary'} />
+              <p className='text text_type_main-default ml-2'>
+                {userName || 'Личный кабинет'}
+              </p>
+            </>
+          )}
         </NavLink>
       </div>
     </nav>
--- src/services/store.test.ts
+++ src/services/store.test.ts
@@ -1,22 +1,12 @@
 import { expect, test } from '@jest/globals';
 import { rootReducer } from './store';
-import { burgerConstructorSlice } from './slices/BurgerConstructorSlice';
-import ingredientsSlice from './slices/IngredientsSlice';
-import { userStateSlice } from './slices/UserinfoSlice';
-import { feedDataSlice } from './slices/FeedDataSlice';
-import { userOrdersHistorySlice } from './slices/UserOrdersHistory';
-import { configureStore } from '@reduxjs/toolkit';
+import store from './store';
 
-const store = configureStore({
-  reducer: {
-    [burgerConstructorSlice.name]: burgerConstructorSlice.reducer,
-    [feedDataSlice.name]: feedDataSlice.reducer,
-    [ingredientsSlice.name]: ingredientsSlice.reducer,
-    [userStateSlice.name]: userStateSlice.reducer,
-    [userOrdersHistorySlice.name]: userOrdersHistorySlice.reducer
-  }
-});
+describe('rootReducer test', () => {
+  test('init state test', () => {
+    const testAction = { type: 'UNKNOWN_ACTION' };
 
-test('test', () => {
-  expect(store).toBeDefined();
+    const initialState = rootReducer(undefined, testAction);
+    expect(initialState).toEqual(store.getState());
+  });
 });
